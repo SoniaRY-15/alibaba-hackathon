41e3f81746695f7a6fb08a66898661f88f056e00
Sonia Renata Yacintaman <soniar.y15@gmail.com>
2026-03-01 18:16:20 +0700

    fixed CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,7 @@ app.use(
       "http://localhost:5500",
       "http://127.0.0.1:5500",
       "http://127.0.0.1:3000",
+      "https://alibaba-hackathon-xi.vercel.app",
     ],
     credentials: true,
   }),
--- frontend/config.js
+++ frontend/config.js
@@ -1,6 +1,6 @@
 // API Configuration
 const API_CONFIG = {
-  BASE_URL: "http://localhost:5000/api",
+  BASE_URL: "https://alibaba-hackathon-xi.vercel.app/api",
 
   endpoints: {
     auth: {
